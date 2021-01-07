631c064e1f38ba8464e1c6c3d3ddcaa11f5f8d61
吴惠兰 <779790228@qq.com>
2021-01-08 00:12:01 +0800

    Initial Commit

---
+++ pages/orderFood/orderFood.js
@@ -0,0 +1,57 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  selectCategory: function(event, ownerInstance) {
+    console.log(event)
+    console.log(ownerInstance)
+    var instance = ownerInstance.selectComponent('.category') // 返回组件的实例
+    console.log(ownerInstance)
+    instance.setClass("selectCategory")
+    return false // 不往上冒泡，相当于调用了同时调用了stopPropagation和preventDefault
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
